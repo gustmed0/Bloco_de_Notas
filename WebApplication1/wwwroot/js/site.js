71d22bb50c786694b833be0998fa336d524cf472
gustmed0 <gustavoolivermed@gmail.com>
2023-08-25 11:17:59 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const noteContent = document.getElementById("note-content");
+
+noteContent.addEventListener("input", function () {
+    localStorage.setItem("savedNote", noteContent.value);
+});
+
+// Recuperar conteúdo salvo ao carregar a página
+window.addEventListener("load", function () {
+    const savedNote = localStorage.getItem("savedNote");
+    if (savedNote) {
+        noteContent.value = savedNote;
+    }
+});
